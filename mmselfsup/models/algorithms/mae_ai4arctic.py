815d4f77ceb8ee7ccdc98f84336583ac90b08f7e
jnoat92-narval <jnoat92@gmail.com>
2025-01-29 14:55:24 -0500

    MAE ai4arctic improved

--- mmselfsup/models/algorithms/mae_ai4arctic.py
+++ mmselfsup/models/algorithms/mae_ai4arctic.py
@@ -1,41 +1,21 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Dict, List, Optional, Tuple
+from typing import List, Optional
 
 import torch
 from mmengine.structures import BaseDataElement
 
 from mmselfsup.registry import MODELS
 from mmselfsup.structures import SelfSupDataSample
-from .base import BaseModel
+from .mae import MAE
 
 
 @MODELS.register_module()
-class MAE_CCH(BaseModel):
+class MAE_CCH(MAE):
     """MAE.
         def reconstruct MODIFIED by No@ FROM mae.py
             IT INCLUDES:
                 - Customized head predicted channels
-
-    Implementation of `Masked Autoencoders Are Scalable Vision Learners
-    <https://arxiv.org/abs/2111.06377>`_.
     """
-
-    def extract_feat(self,
-                     inputs: List[torch.Tensor],
-                     data_samples: Optional[List[SelfSupDataSample]] = None,
-                     **kwarg) -> Tuple[torch.Tensor]:
-        """The forward function to extract features from neck.
-
-        Args:
-            inputs (List[torch.Tensor]): The input images.
-
-        Returns:
-            Tuple[torch.Tensor]: Neck outputs.
-        """
-        latent, mask, ids_restore = self.backbone(inputs[0])
-        pred = self.neck(latent, ids_restore)
-        self.mask = mask
-        return pred
 
     def reconstruct(self,
                     features: torch.Tensor,
@@ -72,24 +52,3 @@ class MAE_CCH(BaseModel):
         results.pred = BaseDataElement(**dict(value=pred))
 
         return results
-
-    def loss(self, inputs: List[torch.Tensor],
-             data_samples: List[SelfSupDataSample],
-             **kwargs) -> Dict[str, torch.Tensor]:
-        """The forward function in training.
-
-        Args:
-            inputs (List[torch.Tensor]): The input images.
-            data_samples (List[SelfSupDataSample]): All elements required
-                during the forward function.
-
-        Returns:
-            Dict[str, torch.Tensor]: A dictionary of loss components.
-        """
-        # ids_restore: the same as that in original repo, which is used
-        # to recover the original order of tokens in decoder.
-        latent, mask, ids_restore = self.backbone(inputs[0])
-        pred = self.neck(latent, ids_restore)
-        loss = self.head(pred, inputs[0], mask)
-        losses = dict(loss=loss)
-        return losses
